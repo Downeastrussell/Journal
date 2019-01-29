bc06e05bf53a734d86576fde613986cf25a4940d
Russell Miller <bet2buy@gmail.com>
2019-01-29 14:44:10 -0500

    added objects to represent journal entries in JS

---
+++ scripts/journal.js
@@ -0,0 +1,46 @@
+var allEntries = [""];
+
+journalEntry1 = {
+    date:"1/7/19" ,
+    subjectsCovered:["curriculum", "HTML & CSS review"],
+    text:"Today was the first day of class, it was a good day", 
+    mood: "entergetic"
+}
+
+journalEntry2 = {
+    date: "1/8/19" ,
+    subjectsCovered:["loops", "conditionals"],
+    text:"It was the second day of class, it was even better" ,
+    mood:"happy" 
+}
+
+
+journalEntry3 = {
+    date: "1/9/19" ,
+    subjectsCovered:["functions", "components"],
+    text: "It was the third day of class, where am i?!" ,
+    mood: "sad"
+}
+
+journalEntry4 = {
+    date: "1/10/19",
+    subjectsCovered:["The DOM?!?!"],
+    text: "it was the 4th day of class, what have i gotten myself into", 
+    mood:"i feel like a lama" 
+}
+
+
+allEntries.push(journalEntry1)
+allEntries.push(journalEntry2)
+allEntries.push(journalEntry3)
+allEntries.push(journalEntry4)
+
+console.log(allEntries)
+
+
+
+
+
+
+
+
