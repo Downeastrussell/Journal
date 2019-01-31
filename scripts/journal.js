ce8d6775e5f449bb9e16c887dde593b3ef160cb3
Russell Miller <bet2buy@gmail.com>
2019-01-31 15:03:22 -0500

    part 3, adding DOM

--- scripts/journal.js
+++ scripts/journal.js
@@ -1,13 +1,13 @@
-var allEntries = [""];
+const allEntries = [];
 
-journalEntry1 = {
+const journalEntry1 = {
     date:"1/7/19" ,
     subjectsCovered:["curriculum", "HTML & CSS review"],
     text:"Today was the first day of class, it was a good day", 
     mood: "entergetic"
 }
 
-journalEntry2 = {
+const journalEntry2 = {
     date: "1/8/19" ,
     subjectsCovered:["loops", "conditionals"],
     text:"It was the second day of class, it was even better" ,
@@ -15,14 +15,14 @@ journalEntry2 = {
 }
 
 
-journalEntry3 = {
+const journalEntry3 = {
     date: "1/9/19" ,
     subjectsCovered:["functions", "components"],
     text: "It was the third day of class, where am i?!" ,
     mood: "sad"
 }
 
-journalEntry4 = {
+const journalEntry4 = {
     date: "1/10/19",
     subjectsCovered:["The DOM?!?!"],
     text: "it was the 4th day of class, what have i gotten myself into", 
@@ -35,7 +35,45 @@ allEntries.push(journalEntry2)
 allEntries.push(journalEntry3)
 allEntries.push(journalEntry4)
 
-console.log(allEntries)
+console.log(allEntries);
+
+
+// document.querySelector("#submitButton").addEventListener("click", (event) => {
+//     const printTextArea = document.querySelector("#journalEntry").value
+//     console.log(printTextArea);
+
+//     const printDate = document.querySelector("#journalDate").value
+//     console.log(printDate)
+
+//     const printSubjectsCovered = document.querySelector("#journalSubjects").value
+//     console.log(printSubjectsCovered)
+
+//     const printJournalValue = document.querySelector("#journalValue").value
+//     console.log(printJournalValue)
+
+//    const print = `<h1>Date:${printDate}</h1><h2>Entry: ${printTextArea}</h2><h2>Subjects Covered: ${printSubjectsCovered}</h2><h2>Mood: ${printJournalValue}`
+//     console.log(print)
+
+//     document.querySelector("#outputArea").innerHTML = print
+
+// })
+
+function makeJournalEntryComponent (arrayOfObjects){
+    for(i=0; i<arrayOfObjects.length; i++){
+    print = `<h1>Date:${arrayOfObjects[i].date}</h1><h2>Entry: ${arrayOfObjects[i].text}</h2><h2>Subjects Covered: ${arrayOfObjects[i].subjectsCovered}</h2><h2>Mood: ${arrayOfObjects[i].mood}`
+
+    document.querySelector("#outputArea").innerHTML += print
+    console.log(print)
+    }
+}
+
+makeJournalEntryComponent(allEntries);
+
+
+
+
+
+
 
 
 
